b00f211ae54003a49b5d387abe905ff87bc7af32
CharlesDelannoy <charles.delannoy@doctolib.com>
2025-10-22 13:26:22 +0200

    add a test

--- postgresql/resource_postgresql_script_test.go
+++ postgresql/resource_postgresql_script_test.go
@@ -1,10 +1,12 @@
 package postgresql
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccPostgresqlScript_basic(t *testing.T) {
@@ -226,4 +228,86 @@ func TestAccPostgresqlScript_timeout(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestAccPostgresqlScript_withDatabase(t *testing.T) {
+	config := `
+	resource "postgresql_database" "test_db" {
+		name = "test_script_db"
+	}
+
+	resource "postgresql_script" "test" {
+		database = postgresql_database.test_db.name
+		commands = [
+			"CREATE TABLE test_table (id INT);",
+			"INSERT INTO test_table VALUES (1);"
+		]
+		depends_on = [postgresql_database.test_db]
+	}
+	`
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("postgresql_script.test", "database", "test_script_db"),
+					resource.TestCheckResourceAttr("postgresql_script.test", "commands.0", "CREATE TABLE test_table (id INT);"),
+					resource.TestCheckResourceAttr("postgresql_script.test", "commands.1", "INSERT INTO test_table VALUES (1);"),
+					testAccCheckTableExistsInDatabase("test_script_db", "test_table"),
+					testAccCheckTableHasRecords("test_script_db", "test_table", 1),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckTableExistsInDatabase(dbName, tableName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*Client)
+		dbClient := client.config.NewClient(dbName)
+		db, err := dbClient.Connect()
+		if err != nil {
+			return fmt.Errorf("Error connecting to database %s: %s", dbName, err)
+		}
+
+		var exists bool
+		query := "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = $1)"
+		err = db.QueryRow(query, tableName).Scan(&exists)
+		if err != nil {
+			return fmt.Errorf("Error checking if table %s exists: %s", tableName, err)
+		}
+
+		if !exists {
+			return fmt.Errorf("Table %s does not exist in database %s", tableName, dbName)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckTableHasRecords(dbName, tableName string, expectedCount int) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*Client)
+		dbClient := client.config.NewClient(dbName)
+		db, err := dbClient.Connect()
+		if err != nil {
+			return fmt.Errorf("Error connecting to database %s: %s", dbName, err)
+		}
+
+		var count int
+		query := fmt.Sprintf("SELECT COUNT(*) FROM %s", tableName)
+		err = db.QueryRow(query).Scan(&count)
+		if err != nil {
+			return fmt.Errorf("Error counting records in table %s: %s", tableName, err)
+		}
+
+		if count != expectedCount {
+			return fmt.Errorf("Expected %d records but got %d in table %s", expectedCount, count, tableName)
+		}
+
+		return nil
+	}
 }
